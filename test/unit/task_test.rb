202b62453c02a3d9617d11b86789a8532df2f136
root <root@localhost.localdomain>
2014-02-22 16:46:41 -0500

    Unit test

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "La tarea debe tener un nombre" do
+    task = Task.new
+    assert !task.save, "La tarea se guardo sin nombre"
+  end
+
+  test "La tarea debe tener un nombre con minimo 6 caracteres" do
+    task = Task.new
+    task.name = "abc"
+    assert !task.save, "La tarea se guardo con un nombre de menos de 6 caracteres"
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "el usuario debe tener un unico nombre de usuario" do
+    user = User.new
+    user.name = users(:cesar).name
+    user.email = "c.orozcomarin@gmail"
+    user.password = "12345678"
+
+    assert !user.save
+  end
 end
