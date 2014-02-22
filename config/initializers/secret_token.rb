192373dd3d79611ad0d5437848f683f44f026646
Luis Edo Gonzalez <luego1986@gmail.com>
2014-02-22 16:07:58 -0500

    Foundation and Devise Installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManagerTest::Application.config.secret_token = '9fb740576521ce9c44eb5e3922c4e20f231c183f832342355b26a6c58c80c26dece34a72182bc6aa3e555b273d0e79cb4aa61cc0a5731d81bac6f9a969f3b981'
