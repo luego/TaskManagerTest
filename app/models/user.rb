711d8b8d3ea008e5c9d3ecdcf925f30bec69312a
Luis Edo Gonzalez <luego1986@gmail.com>
2014-02-22 16:54:52 -0500

    Create Scaffold and devise changes fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :email, :last_name, :name
 
-  validates :email, presence : true
-  validates :name, presence : true
-  validates :last_name, presence : true
+  validates :name, presence: true
+  validates :last_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 TaskManagerTest::Application.routes.draw do
+  resources :tasks
+
+
   devise_for :users
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+    get "register", to: "devise/registrations#new", as: :register
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
