ae3f338e03f49b57b841cc1c686b53b0c16aaaf5
root <root@localhost.localdomain>
2014-02-22 16:09:17 -0500

    Creacion y Validacion de task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :done, :name
+  attr_accessible :done, :name, :user_id, :created_at
+  validates :user, presence: true
+  validates :name, presence: true, length: {minimum: 6}
 end
